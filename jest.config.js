284559118698a32b81ec4fb3475be81216acf4b7
Franklin Chieze <chieze.franklin@gmail.com>
2020-05-21 22:22:04 +0100

    set up React + TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  preset: 'ts-jest',
-  testRegex: '(<rootDir>/test/.*|(\\.|/)(test|spec))\\.tsx?$',
-  modulePaths: ['<rootDir>/src'],
-  setupFiles: [
-    '<rootDir>/test/__mocks__/phenix-web-packages'
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.tsx',
-    '<rootDir>/src/**/*.ts'
-  ],
-  clearMocks: true,
-  restoreMocks: true,
-  coverageDirectory: '<rootDir>/coverage/',
-  setupFilesAfterEnv: ['<rootDir>/test/setupEnzyme.ts'],
-  transform: {'^.+\\.js$': 'ts-jest'},
-  moduleNameMapper: {'\\.css$': 'identity-obj-proxy'}
-};
+    preset: 'ts-jest',
+    testRegex: '(<rootDir>/test/.*|(\\.|/)(test|spec))\\.tsx?$',
+    modulePaths: ['<rootDir>/src'],
+    setupFiles: [
+      '<rootDir>/test/__mocks__/phenix-web-packages'
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.tsx',
+      '<rootDir>/src/**/*.ts'
+    ],
+    clearMocks: true,
+    restoreMocks: true,
+    coverageDirectory: '<rootDir>/coverage/',
+    setupFilesAfterEnv: ['<rootDir>/test/setupEnzyme.ts'],
+    transform: {'^.+\\.js$': 'ts-jest'},
+    moduleNameMapper: {'\\.css$': 'identity-obj-proxy'}
+  };
+  
